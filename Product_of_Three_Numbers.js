ce3dc193a739d9beee884ae397f352715915dbbf
Ferdaus Zaman Polok <ferdauszamanpolok@Ferdauss-Mac-mini.local>
2022-11-09 17:48:40 +0600

    C. Product of Three Numbers

---
+++ Product_of_Three_Numbers.js
@@ -0,0 +1,51 @@
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+let standardtInputSting = "";
+let currentLine = 0;
+function readLine() {
+  return standardtInputSting[currentLine++];
+}
+process.stdin.on("data", (rawData) => {
+  standardtInputSting += rawData;
+});
+process.stdin.on("end", (_) => {
+  standardtInputSting = standardtInputSting
+    .trim()
+    .split("\n")
+    .map((line) => {
+      return line.trim();
+    });
+  main();
+});
+
+function main() {
+    var n = readLine();
+    n = parseInt(n);
+    N= n
+    counter= 0
+    arr1= []
+
+    for(i=2; i*i <= N; i++){
+        if (n % i==0){
+            counter++
+            n= n/i
+            arr1.push(i)
+        }
+        if (counter==2){
+            arr1.push(n)
+            break
+        }
+    }
+
+    if (counter<2){
+        console.log("No")
+    }
+    else if(arr1[0] == arr1[2] || arr1[1] == arr1[2] ){
+        console.log("No")
+    }
+    else{
+        console.log("Yes")
+        console.log(arr1)
+    }
+
+}
---
+++ Product_of_Three_Numbers_browser.js
@@ -0,0 +1,29 @@
+tc = prompt();
+
+for (let i = 0; i < tc; i++) {
+    n = parseInt(prompt());
+    N = n;
+    counter = 0;
+    arr1 = [];
+
+    for (i = 2; i * i <= N; i++) {
+        if (n % i == 0) {
+        counter++;
+        n = n / i;
+        arr1.push(i);
+        }
+        if (counter == 2) {
+        arr1.push(n);
+        break;
+        }
+    }
+
+    if (counter < 2) {
+        console.log("No");
+    } else if (arr1[0] == arr1[2] || arr1[1] == arr1[2]) {
+        console.log("No");
+    } else {
+        console.log("Yes");
+        console.log(arr1);
+    }
+}
